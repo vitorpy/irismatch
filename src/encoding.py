64697b4ea911ca427130dc1216385ab065b3049d
Vitor Braga <vitorpybraga@gmail.com>
2014-03-08 14:49:03 -0300

    Messing around with edge detection

--- src/encoding.py
+++ src/encoding.py
@@ -2,15 +2,32 @@
 
 from PIL import Image
 from iris_detection import detect_iris
+from scipy.ndimage import filters
+import matplotlib.pyplot as plt
 
 def show(filename):
     im = Image.open(filename)
     im.show()
 
+def edge_detect_experiment(filename):
+    raw_image = plt.imread(filename)
+    raw_image = raw_image[:,:,0] # get the first channel
+
+    plt.imshow(raw_image)
+    plt.show()
+    
+    image = filters.sobel(raw_image, 0)**2 + filters.sobel(raw_image, 1)**2 
+
+    plt.imshow(image)
+    plt.show()    
+
 if __name__ == '__main__':
     print "[*] irismatch Enconding"
     filename = "../working-db/003R_3.png"
+    
+    edge_detect_experiment(filename)
+    
     #show(filename)
-    detect_iris(filename)
+    #detect_iris(filename)
     
 
