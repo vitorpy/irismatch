5ab0309aabc0dfaac1a6a15f5205ae305d377349
Vitor Braga <vitorpybraga@gmail.com>
2014-03-08 16:14:35 -0300

    Testing alternatives for edge detection.

--- src/encoding.py
+++ src/encoding.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python2.7
 
-from PIL import Image
+from PIL import Image, ImageFilter
 from iris_detection import detect_iris
 from scipy.ndimage import filters
 import matplotlib.pyplot as plt
+from scipy.misc import toimage
 
 def show(filename):
     im = Image.open(filename)
@@ -13,13 +14,17 @@ def edge_detect_experiment(filename):
     raw_image = plt.imread(filename)
     raw_image = raw_image[:,:,0] # get the first channel
 
-    plt.imshow(raw_image)
-    plt.show()
-    
-    image = filters.sobel(raw_image, 0)**2 + filters.sobel(raw_image, 1)**2 
+    #plt.imshow(raw_image)
+    #plt.show()
 
-    plt.imshow(image)
-    plt.show()    
+    #image = filters.sobel(raw_image, 0)**2 + filters.sobel(raw_image, 1)**2
+    image = toimage(raw_image)
+    image = image.filter(ImageFilter.FIND_EDGES)
+
+    image.show()
+
+    #plt.imshow(image)
+    #plt.show()    
 
 if __name__ == '__main__':
     print "[*] irismatch Enconding"
